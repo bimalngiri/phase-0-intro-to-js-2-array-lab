85e8ad1556e49f1b738b3f798f6d31248fe1b4d8
Bimal Giri <bimalbishu2040@gmail.com>
2023-03-08 19:41:40 -0500

    Lab Complet

--- index.js
+++ index.js
@@ -1,1 +1,58 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+cats.length = 0;
+
+cats.push("Milo", "Otis", "Garfield");
+
+
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+    
+}
+
+
+
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+    
+  }
+  
+
+
+  function destructivelyRemoveLastCat(){
+    cats.pop();
+    
+  }
+  
+
+  function destructivelyRemoveFirstCat(name){
+    cats.shift(name);
+    
+  }
+  destructivelyRemoveFirstCat.shift();
+
+  function appendCat(name) {
+    const name_1 = [...cats, name];
+    return name_1;
+  }
+
+ 
+ console.log(cats);
+
+  function prependCat(name){
+    const name_2=[name, ...cats];
+    return name_2
+ } 
+
+ function removeLastCat(){
+    const caats = cats.slice(0,cats.length-1);
+    return caats;
+  }
+  
+  function removeFirstCat(){
+    const kitten = cats.slice(1)
+    return kitten;
+  }
+        
